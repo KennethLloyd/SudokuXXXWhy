4123386022159eb03af3024d275c4aa117d5e626
KennethLloyd <kpverastigue@up.edu.ph>
2018-04-28 18:06:17 +0800

    added cell constraints in input values

---
+++ MyIntFilter.java
@@ -0,0 +1,72 @@
+import javax.swing.text.*;
+
+public class MyIntFilter extends DocumentFilter {
+	int max;
+	
+	public MyIntFilter(int max) {
+		this.max = max; //set max value
+	}
+   @Override
+   public void insertString(FilterBypass fb, int offset, String string,
+         AttributeSet attr) throws BadLocationException {
+
+      Document doc = fb.getDocument();
+      StringBuilder sb = new StringBuilder();
+      sb.append(doc.getText(0, doc.getLength()));
+      sb.insert(offset, string);
+
+      if (test(sb.toString())) {
+         super.insertString(fb, offset, string, attr);
+      } else {
+         // warn the user and don't allow the insert
+      }
+   }
+
+   private boolean test(String text) {
+	  if (text.trim().isEmpty()) { //the user want to empty the cell
+		  return true; //allow
+	  }
+      try {
+         int num = Integer.parseInt(text);
+         if (num > max) { //val should be less than or equal to the max value
+        	 return false;
+         }
+         return true;
+      } catch (NumberFormatException e) {
+         return false;
+      }
+   }
+
+   @Override
+   public void replace(FilterBypass fb, int offset, int length, String text,
+         AttributeSet attrs) throws BadLocationException {
+
+      Document doc = fb.getDocument();
+      StringBuilder sb = new StringBuilder();
+      sb.append(doc.getText(0, doc.getLength()));
+      sb.replace(offset, offset + length, text);
+
+      if (test(sb.toString())) {
+         super.replace(fb, offset, length, text, attrs);
+      } else {
+         // warn the user and don't allow the insert
+      }
+
+   }
+
+   @Override
+   public void remove(FilterBypass fb, int offset, int length)
+         throws BadLocationException {
+      Document doc = fb.getDocument();
+      StringBuilder sb = new StringBuilder();
+      sb.append(doc.getText(0, doc.getLength()));
+      sb.delete(offset, offset + length);
+
+      if (test(sb.toString())) {
+         super.remove(fb, offset, length);
+      } else {
+         // warn the user and don't allow the insert
+      }
+
+   }
+}
